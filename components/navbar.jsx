51f36111a6bc9c03a9d80e9bbf6b47209e7e698e
Collins-Klo <chemiteicollins@gmail.com>
2025-02-22 01:34:58 +0300

    Finished with navigation

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -23,13 +23,37 @@ const Icon = ({styles, name, imgUrl, isActive, disabled, handleClick }) => (
 const Nav = () => {
 
     const [isActive, setIsActive] = useState('dashboard');
+    const [isLink, setIsLink] = useState('dashboard');
     return (
-      <div className="flex justify-between items-center flex-row sticky bottom-3 w-3/4 "> 
-        <Link href="/">
+      <div className="flex justify-between items-center flex-row sticky bottom-3 w-3/4 m-auto"> 
+        {/* <Link href="/">
             <Icon styles="w-[40px] h-[40px] bg-[#2c2f32]" imgUrl={logo} />
         
-        </Link>
+        </Link> */}
 
+        <div className='flex-1 flex flex-row justify-between
+        items-center bg-[#1c1c24] rounded-[15px] w-auto
+        py-3 px-6'>
+            <div className='flex flex-row justify-center items-center
+            gap-3'>
+                {navlinks.map((link) => (
+                        <Link 
+                            key={link.name} 
+                            href={link.link}
+                        >
+                            <Icon
+                                {...link} 
+                                isActive={isActive} 
+                                handleClick={() => {
+                                    if (!link.disabled) {
+                                        setIsActive(link.name);
+                                    }
+                                }} 
+                            />
+                        </Link>
+                    ))}
+            </div>
+        </div>
       </div>
     )
   }
